ce8b7b7158566201e78100e44ddb14e9b9577f98
Div685 <divyeshpatel685@gmail.com>
2022-06-13 16:59:31 +0300

    Add delete items function
    - Add toggle method to check and uncheck value

--- src/containers/ProductList.js
+++ src/containers/ProductList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-import { getData } from '../api/fetch';
+import { deleteData, getData } from '../api/fetch';
 import Header from '../components/Header';
 
 const ProductList = () => {
@@ -26,22 +26,45 @@ const ProductList = () => {
     })
   }, []);
 
-  const handleClear = () => {
-    let arrayids = [];
-    productState.forEach(d => {
-        arrayids.push(d);
-    });
+  const handleClear = async () => {
+    if(productState.length > 0) {
+
+      const pS = productState.toString();
+      const data = new FormData();
+      data.append("checkbox[]", [pS]);
+      const url = "http://localhost:8000/api/delete.php";
+      // const req = await fetch(url, { mode: 'cors', method: 'POST', body: data})
+      //   .then(res => res)
+      //   .catch((e) => ('Error: ', e));
+      const req = await deleteData(data);
+      console.log(req);
+
+    } else {
+      console.log('Please Select Item to delete')
+    }
+  }
+
+
+  const toggleOne = (checked, id) => {
+    if(checked) {
+      setProductState(productState.concat([id]));
+    } else {
+      setProductState(productState.filter((el) => el !== id))
+    }
   }
 
   const onCheckBoxChange = (event) => {
-    const productStates = [...productState];
-    const isChecked = productStates.includes(event.target.checked);
-    if (!isChecked) {
-      productStates.push(event.target.value);
-    } else {
-      productStates.splice(productState.indexOf(event.target.value), 1);
-    }
-    setProductState(productStates);
+    // const productStates = [...productState];
+    // const isChecked = productStates.includes(event.target.checked);
+    // if (!isChecked) {
+    //   productStates.push(event.target.value);
+    // } else {
+    //   productStates.splice(productState.indexOf(event.target.value), 1);
+    // }
+    // setProductState(productStates);
+
+    toggleOne(event.target.checked, event.target.value);
+    
   }
 
   return (
@@ -52,21 +75,25 @@ const ProductList = () => {
           itemData && itemData.length ?
           itemData.map((item) => (
             <div className="items-data" key={item.SKU}>
-              <input className="delete-checkbox" 
-                type="checkbox"
-                value={item.SKU}
-                name="checkbox" 
-                id="checkbox"
-                onChange={ 
-                  onCheckBoxChange
-                }
-                 />
-              <p className="sku"> {item.SKU} </p>
-              <p className="name">{item.Name}</p>
-              <p className="price"> {item.Price} $</p>
-              <p className="measure"> {item.Measure} </p>
+              <form method="post">
+
+                <input className="delete-checkbox" 
+                  type="checkbox"
+                  value={item.pid}
+                  name="checkbox[]" 
+                  id="checkbox"
+                  checked={(productState && productState.indexOf(item.pid)) >= 0}
+                  onChange={ 
+                    onCheckBoxChange
+                  }
+                  />
+                <p className="sku"> {item.SKU} </p>
+                <p className="name">{item.Name}</p>
+                <p className="price"> {item.Price} $</p>
+                <p className="measure"> {item.Measure} </p>
+              </form>
             </div>
-          )) : (<h1>No Data</h1>)
+          )) : (<h1>Loading...</h1>)
         }
       </section>
     </main>
