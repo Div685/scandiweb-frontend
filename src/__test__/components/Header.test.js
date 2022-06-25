e5ef07910ec1c0336fc9949e732e458934f23a02
Div685 <divyeshpatel685@gmail.com>
2022-06-25 16:57:41 +0300

    Add test for components
    - Product form
    - Header

---
+++ src/__test__/components/Header.test.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import '@testing-library/jest-dom';
+import Header from "../../components/Header";
+
+  beforeEach(() => {
+  //   // setup a DOM element as a render target
+  let title = 'Product List';
+  render(<Header title={title} />);
+  });
+
+describe('Header Component', () => {
+  it('render Header Page correctly', () => {
+    const tree = renderer.create(
+          <Header />
+    );
+    expect(tree).toMatchSnapshot();
+  })
+})
+
+describe('Header ', ()=> {
+  it('render Header Title', ()=> {
+    expect(screen.getByRole('heading').textContent).toBe(' Product List ');
+  })
+});
---
+++ src/__test__/components/ProductForm.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import '@testing-library/jest-dom';
+import { MemoryRouter } from 'react-router-dom';
+import ProductForm from '../../containers/ProductForm';
+
+  describe('Proudct Form Component', () => {
+    it('render Product Form Page correctly', () => {
+      const tree = renderer.create(
+        <MemoryRouter>
+            <ProductForm />
+        </MemoryRouter>
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+    })
+  })
