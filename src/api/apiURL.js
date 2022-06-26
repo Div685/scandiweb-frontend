b6b691e87e5eda7175c1b4298b45c4f09e83e3dc
Div685 <divyeshpatel685@gmail.com>
2022-06-26 18:26:22 +0300

    Remove comment code.

--- src/api/apiURL.js
+++ src/api/apiURL.js
@@ -1,5 +1,3 @@
 const apiUrl = 'https://scandiproducts1.000webhostapp.com/php/api/';
-// const apiUrl = 'http://localhost:8000/api/';
-// const apiUrl = 'http://scandiwebphp.epizy.com/api/';
 
 export default apiUrl;
