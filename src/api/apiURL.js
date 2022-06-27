78c0719af92548ef96372f160dd92dfa528b5184
Div685 <divyeshpatel685@gmail.com>
2022-06-27 13:36:26 +0300

    Update APIURL

--- src/api/apiURL.js
+++ src/api/apiURL.js
@@ -1,3 +1,4 @@
-const apiUrl = 'https://scandiproducts1.000webhostapp.com/php/api/';
+// const apiUrl = 'https://scandiproducts1.000webhostapp.com/php/api/';
+const apiUrl = 'https://protonic-instructor.000webhostapp.com/php/api/';
 
 export default apiUrl;
